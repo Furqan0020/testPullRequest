4d1e8447c822b75a88132ecbfc765c0b3f878e18
furqan0020 <furqanazeem416@gmail.com>
2025-12-12 15:32:41 +0500

    add code for multiply branch

--- calculator.js
+++ calculator.js
@@ -23,3 +23,11 @@ if (require.main === module) {
   console.log(`2 + 3 = ${add(2, 3)}`);
   console.log(`5 - 2 = ${subtract(5, 2)}`);
 }
+
+  // --- NEW ADDITION ---
+  function multiply(a, b) {
+    return a * b;
+  }
+  // --------------------
+  
+  module.exports = { add, subtract, multiply };
